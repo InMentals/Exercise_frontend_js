1c77bd44529a0f6355370cc2e86637c0259bdb79
Pabo Cervigon <pablocervigon@gmail.com>
2025-04-06 03:39:01 +0200

    refactored for using MVC structure

---
+++ show-ads/showAdsController.js
@@ -0,0 +1,13 @@
+import { ads } from './showAdsModel.js'
+import { buildAd } from './showAdsView.js';
+
+export function showAdsController() {
+  
+  const container = document.querySelector(".adv-container");
+
+  ads.forEach((ad) => {
+    const adHtml = document.createElement("div");
+    adHtml.innerHTML = buildAd(ad);
+    container.appendChild(adHtml);
+  })
+}
