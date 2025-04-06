5496b9573fe659f211ba72aa43c4c90a9231fd10
Pabo Cervigon <pablocervigon@gmail.com>
2025-04-06 04:05:56 +0200

    errors handled

--- show-ads/showAdsController.js
+++ show-ads/showAdsController.js
@@ -2,19 +2,23 @@ import { getAds } from './showAdsModel.js'
 import { buildAd, buildNoAdsAdvice } from './showAdsView.js';
 
 export async function showAdsController() {
-  
-  const container = document.querySelector(".adv-container");
-  const ads = await getAds();
-  if (ads.length > 0) {
-    drawAds(ads, container);
-  }else {
-    container.innerHTML = buildNoAdsAdvice();
+
+  try {
+    const ads = await getAds();
+    drawAds(ads);
+  } catch (error) {
+    alert(error.message);
   }
 }
 
 
-function drawAds(ads, container) {
+
+function drawAds(ads) {
+  const container = document.querySelector(".adv-container");
   container.innerHTML = ""; 
+  if (ads.length === 0) {
+    container.innerHTML = buildNoAdsAdvice();
+  }
   ads.forEach((ad) => {
     const adHtml = document.createElement("div");
     adHtml.innerHTML = buildAd(ad);
