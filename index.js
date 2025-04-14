06bea85e30b085f25838b6b8dd28828874571835
Pabo Cervigon <pablocervigon@gmail.com>
2025-04-14 19:21:06 +0200

    notifications controller boilerplate

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { showProductsController } from "./show-products/showProductsController.js";
-
 import { loaderController } from "./loader/loaderController.js";
+import { notificationsController } from "./notifiations/notificationsController.js";    
 
 document.addEventListener("DOMContentLoaded", () => {
 
     const productsContainer = document.querySelector(".products-container");
     const loaderContainer = document.querySelector(".loader-container");
+    const notificationsContainer = document.querySelector(".notifications-container");
     const {show, hide} = loaderController(loaderContainer);
 
     productsContainer.addEventListener('load-products-started', () => {
@@ -16,5 +17,6 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     showProductsController(productsContainer);
+    notificationsController(notificationsContainer);
 
 })
